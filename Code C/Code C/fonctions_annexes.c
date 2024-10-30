6e688af19cdfabcf27a6c313abd50114b5fdef18
MathiasSSC <114427713+MathiasSSC@users.noreply.github.com>
2024-10-30 21:38:05 +0100

    Add comments fonctions_annexes.c

--- Code C/Code C/fonctions_annexes.c
+++ Code C/Code C/fonctions_annexes.c
@@ -8,6 +8,10 @@
 /* Fonctions annexes partie 1*/
 
 void formater_key(char * key){
+  /* Fonction prennant une chaine de caractère et la transforme 
+  en une chaine de taille LEN_BLOC soit en troquant ou en 
+  répétant la la chaine de caractère. */
+  
   int taille_key = strlen(key);
   char new_key[LEN_BLOC+1];
   for (int i =0; i< LEN_BLOC ; i++){
@@ -18,6 +22,8 @@ void formater_key(char * key){
 }
 
 void afficher_bloc_hex(const char *bloc, size_t taille) {
+    // Fonction transformant une chaine de caractère en un code hexadécimal
+  
     printf("Bloc : ");
     for (size_t i = 0; i < taille; i++) {
         printf("%02X ", (unsigned char)bloc[i]);
